3a77535bcabfe2f2e3fc357bbc3d49497d0b081b
Athos Benther <athum@live.com>
2024-04-10 19:40:03 -0300

    #20 Nightly

--- BRIE/Controls/CanvasLayerControl.xaml.cs
+++ BRIE/Controls/CanvasLayerControl.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.ComponentModel;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 
@@ -7,15 +8,20 @@ namespace BRIE.Controls
     /// <summary>
     /// Interaction logic for CanvasLayerControl.xaml
     /// </summary>
-    public partial class CanvasLayerControl : UserControl
+    public partial class CanvasLayerControl : UserControl, INotifyPropertyChanged
     {
         public static readonly DependencyProperty TargetCanvasProperty = DependencyProperty.Register(
             "TargetCanvas", typeof(Canvas), typeof(CanvasLayerControl), new PropertyMetadata(null));
         public Canvas TargetCanvas
         {
             get { return (Canvas)GetValue(TargetCanvasProperty); }
-            set { SetValue(TargetCanvasProperty, value); }
+            set
+            {
+                SetValue(TargetCanvasProperty, value);
+                OnPropertyChanged(nameof(TargetCanvas));
+            }
         }
+
         public bool CanOpenFile
         {
             get { return (bool)GetValue(CanOpenFileProperty); }
@@ -23,14 +29,12 @@ namespace BRIE.Controls
             {
                 btnOpenFile.Visibility = value ? Visibility.Visible : Visibility.Collapsed;
                 SetValue(CanOpenFileProperty, value);
+                OnPropertyChanged(nameof(CanOpenFile));
             }
         }
 
-        // Using a DependencyProperty as the backing store for CanOpenFile.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty CanOpenFileProperty =
             DependencyProperty.Register("CanOpen", typeof(bool), typeof(CanvasLayerControl), new PropertyMetadata(null));
-
-
 
         public bool CanSaveFile
         {
@@ -40,14 +44,12 @@ namespace BRIE.Controls
                 btnSaveFile.Visibility = value ? Visibility.Visible : Visibility.Collapsed;
                 btnSaveCopyFile.Visibility = value ? Visibility.Visible : Visibility.Collapsed;
                 SetValue(CanSaveFileProperty, value);
+                OnPropertyChanged(nameof(CanSaveFile));
             }
         }
 
-        // Using a DependencyProperty as the backing store for CanSave.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty CanSaveFileProperty =
             DependencyProperty.Register("CanSave", typeof(bool), typeof(CanvasLayerControl), new PropertyMetadata(null));
-
-
 
         public bool CanExportFile
         {
@@ -56,26 +58,63 @@ namespace BRIE.Controls
             {
                 btnExportFile.Visibility = value ? Visibility.Visible : Visibility.Collapsed;
                 SetValue(CanExportFileProperty, value);
+                OnPropertyChanged(nameof(CanExportFile));
             }
         }
 
-        // Using a DependencyProperty as the backing store for CanExportFile.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty CanExportFileProperty =
             DependencyProperty.Register("CanExport", typeof(bool), typeof(CanvasLayerControl), new PropertyMetadata(null));
 
+        public CanvasLayerControl? ParentLayer
+        {
+            get { return (CanvasLayerControl)GetValue(ParentLayerProperty); }
+            set
+            {
+                SetValue(ParentLayerProperty, value);
+                OnPropertyChanged(nameof(ParentLayer));
+            }
+        }
 
+        public static readonly DependencyProperty? ParentLayerProperty =
+            DependencyProperty.Register("ParentLayer", typeof(CanvasLayerControl), typeof(CanvasLayerControl), null);
 
-        public event RoutedEventHandler OpenFileClick;
-        public event RoutedEventHandler SaveFileClick;
-        public event RoutedEventHandler SaveCopyFileClick;
-        public event RoutedEventHandler ExportFileClick;
-        public string Label { get; set; }
+        public bool IsLayerVisible
+        {
+            get { return TargetCanvas.Visibility == Visibility.Visible; }
+            set
+            {
+                SetValue(IsLayerVisibleProperty, value);
+                TargetCanvas.Visibility = value ? Visibility.Visible : Visibility.Collapsed;
+                btnLayerVisibility.Foreground = value ? Brushes.Black : Brushes.Red;
+                btnLayerVisibility.Content = value ? "\xE7B3" : "\xE738";
+                OnPropertyChanged(nameof(IsLayerVisible));
+            }
+        }
+
+        public static readonly DependencyProperty IsLayerVisibleProperty =
+            DependencyProperty.Register("IsLayerVisible", typeof(bool), typeof(CanvasLayerControl), new PropertyMetadata(true));
+
+        public event RoutedEventHandler? OpenFileClick;
+        public event RoutedEventHandler? SaveFileClick;
+        public event RoutedEventHandler? SaveCopyFileClick;
+        public event RoutedEventHandler? ExportFileClick;
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string? Label { get; set; }
 
 
         public CanvasLayerControl()
         {
             InitializeComponent();
             DataContext = this;
+        }
+
+        private void Parent_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+        {
+            if(e.PropertyName == nameof(IsLayerVisible) && ParentLayer != null) {
+                IsEnabled = ParentLayer.IsLayerVisible;
+                TargetCanvas.Visibility = ParentLayer.IsLayerVisible? Visibility.Visible : Visibility.Collapsed;
+            }
         }
 
         private void OpenFile_Click(object sender, System.Windows.RoutedEventArgs e)
@@ -85,13 +124,7 @@ namespace BRIE.Controls
 
         private void btnLayerVisibility_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-            Button source = (Button)sender;
-
-            bool isVisible = TargetCanvas.Visibility == System.Windows.Visibility.Visible;
-
-            TargetCanvas.Visibility = isVisible ? System.Windows.Visibility.Collapsed : System.Windows.Visibility.Visible;
-            source.Foreground = isVisible ? Brushes.Red : Brushes.Black;
-            source.Content = isVisible ? "\xE738" : "\xE7B3";
+            IsLayerVisible = !IsLayerVisible;
         }
 
         private void SaveFile_Click(object sender, RoutedEventArgs e)
@@ -107,6 +140,11 @@ namespace BRIE.Controls
         private void ExportFile_Click(object sender, RoutedEventArgs e)
         {
             ExportFileClick?.Invoke(this, e);
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
