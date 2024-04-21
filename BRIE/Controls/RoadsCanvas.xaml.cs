2a1e9a5da048998d56fb0e941f449bfaea24f2f4
Athos Benther <athum@live.com>
2024-04-21 09:44:54 -0300

    Advances #7 Create controls for Node editing

---
+++ BRIE/Controls/RoadsCanvas.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Controls;
+using BRIE.Classes.Statics;
+
+namespace BRIE.Controls
+{
+    /// <summary>
+    /// Interaction logic for RoadsCanvas.xaml
+    /// </summary>
+    public partial class RoadsCanvas : UserControl
+    {
+
+        public RoadsCanvas()
+        {
+            InitializeComponent();
+
+            roadsCanvas.Height = Project.Resolution;
+            roadsCanvas.Width = Project.Resolution;
+
+        }
+    }
+}
